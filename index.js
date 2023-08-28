7012fc1fabf98a4860c40754cbf6d358df46de68
Sean <seanoinca@gmail.com>
2023-08-28 13:24:49 -0700

    Gitignore

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
-const express = require('express')
-const http = require('http');
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socketIo = require('socket.io');
-var cors = require("cors")
-const session = require('express-session');
+const dotenv = require("dotenv");
+dotenv.config();
+var cors = require("cors");
+const session = require("express-session");
 const store = new session.MemoryStore();
-const UserRouter = require('./Routes/Users.js')
-const CableRouter = require('./Routes/Cables')
-const passport = require('passport');
-const cookieParser = require('cookie-parser')
-const LocalStrategy = require('passport-local').Strategy;
-var config = require('./config.json');
+const UserRouter = require("./Routes/Users.js");
+const CableRouter = require("./Routes/Cables");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const LocalStrategy = require("passport-local").Strategy;
+var config = require("./config.json");
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 app.use(cookieParser());
-app.use(session({
-  secret: 'somesecret',
-  cookie: {maxAge:4000000},
-  saveUninitialized: false,
-  resave:false,
-  store
-}));
-
-//connect to oracle db 
-const oracledb = require('oracledb');
+app.use(
+  session({
+    secret: "somesecret",
+    cookie: { maxAge: 4000000 },
+    saveUninitialized: false,
+    resave: false,
+    store,
+  })
+);
+console.log(
+  process.env.USER + process.env.CONNECTSTRING + process.env.PASSWORD
+);
+//connect to oracle db
+const oracledb = require("oracledb");
 try {
-  oracledb.initOracleClient({libDir: 'instantclient_21_6'});
+  oracledb.initOracleClient({ libDir: "instantclient_21_6" });
 } catch (err) {
   console.error(err);
   process.exit(1);
 }
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 const conDetails = {
-  user          : config.user,
-  password      : config.password,
-  connectString : config.connectString,
-  events:true
-}
-
-
-
-
-
-
+  user: process.env.USER.toString(),
+  password: process.env.PASSWORD.toString(),
+  connectString: process.env.CONNECTSTRING.toString(),
+  events: true,
+};
 
 passport.serializeUser((user, done) => {
   done(null, user.USERNAME);
 });
 passport.deserializeUser(async (username, done) => {
-  console.log("DESERIALIZE")
+  console.log("DESERIALIZE");
   let db;
   try {
     db = await oracledb.getConnection(conDetails);
-   
+
     const result = await db.execute(
       `SELECT * FROM SMARTCAPTAR_USERS WHERE USERNAME = '${username}'`
     );
-   
-    if(result.rows.length > 0){
-     done(null,result.rows[0])
+
+    if (result.rows.length > 0) {
+      done(null, result.rows[0]);
     }
   } catch (err) {
-    done(err,null)
+    done(err, null);
   }
 
   if (db) {
@@ -75,92 +76,88 @@ passport.deserializeUser(async (username, done) => {
   }
 });
 
-passport.use(new LocalStrategy(
- async (username, password, done) => {
-  console.log(username + password)
-  let db;
-  try {
-  db = await oracledb.getConnection(conDetails);
-    
-    const result = await db.execute(
-      `SELECT * FROM SMARTCAPTAR_USERS WHERE USERNAME = '${username}'`
-    );
-   
-    if(result.rows.length > 0){
-     
-     done(null,result.rows[0])
-    }else{
-      done(null,false)
-    }
-  } catch (err) {
-    done(err,null);
-  }
-
-  if (db) {
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    console.log(username + password);
+    let db;
     try {
-      await db.close();
+      db = await oracledb.getConnection(conDetails);
+
+      const result = await db.execute(
+        `SELECT * FROM SMARTCAPTAR_USERS WHERE USERNAME = '${username}'`
+      );
+
+      if (result.rows.length > 0) {
+        done(null, result.rows[0]);
+      } else {
+        done(null, false);
+      }
     } catch (err) {
-      console.error(err);
+      done(err, null);
     }
-  }
- }
-));
+
+    if (db) {
+      try {
+        await db.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.use((req,res,next) => {
-
+app.use((req, res, next) => {
   req.db = oracledb;
-  console.log(req.isAuthenticated())
-  if(req.isAuthenticated()){
-    console.log("USER:")
-    console.log(req.user)
-  }else{
-    console.log("NO")
+  console.log(req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    console.log("USER:");
+    console.log(req.user);
+  } else {
+    console.log("NO");
   }
   req.conDetails = conDetails;
-  
+
   console.log(`${req.method} - ${req.url}`);
   return next();
-})
+});
 
-app.use('/',UserRouter);
-app.use('/',CableRouter);
-app.post('/login', cors(),passport.authenticate('local'), (req,res) => {
+app.use("/", UserRouter);
+app.use("/", CableRouter);
+app.post("/login", cors(), passport.authenticate("local"), (req, res) => {
   res.status(200).json(req.user);
 });
-app.post('/logout', cors(), (req,res) => {
-  console.log("YEET")
-  req.logout(function(err) {
-    if (err) { return next(err); }
+app.post("/logout", cors(), (req, res) => {
+  console.log("YEET");
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
   });
-  res.send(200)
-  console.log(req.isAuthenticated())
-
+  res.send(200);
+  console.log(req.isAuthenticated());
 });
 
-
-
-
-app.post("/test", async (req,res) => {
-  //New variable for cables taht are being sent 
-  let cablesUpload = req.body.conn
-  let user= req.body.user
+app.post("/test", async (req, res) => {
+  //New variable for cables taht are being sent
+  let cablesUpload = req.body.conn;
+  let user = req.body.user;
   oracledb.autoCommit = true;
-    let db;
-  try {   
+  let db;
+  try {
     db = await oracledb.getConnection(conDetails);
-     for(var i = 0; i < cablesUpload.length; i++){
+    for (var i = 0; i < cablesUpload.length; i++) {
       const result = await db.execute(
         `INSERT INTO SMARTCAPTAR_COMPATIBILITY (CABLETYPE,COMPAT)
         VALUES 
         (
         '${cablesUpload[i].CATNO}',
         '${cablesUpload[i].arr.toString()}'
-        )`);
+        )`
+      );
     }
-    res.json({msg: "SUCCESS"})
+    res.json({ msg: "SUCCESS" });
   } catch (err) {
     console.error(err);
   }
@@ -172,18 +169,12 @@ app.post("/test", async (req,res) => {
       console.error(err);
     }
   }
-})
-
-
-
-
-
-
+});
 
 // Starting Web Server
 async function ws() {
-  server.listen(5000, function(){
-    console.log('Listening on http://localhost:' + 5000);
+  server.listen(5000, function () {
+    console.log("Listening on http://localhost:" + 5000);
   });
 }
 
